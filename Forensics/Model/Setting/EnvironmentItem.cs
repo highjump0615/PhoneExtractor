6bcd5884162843b86507e837e0059f9860a1589e
highjump <lixiang901112@163.com>
2017-09-22 10:59:52 +0800

    Added UI for environment check page

---
+++ Forensics/Model/Setting/EnvironmentItem.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forensics.Model.Setting
+{
+    public class EnvironmentItem
+    {
+        public enum StatusEnum
+        {
+            INSTALLING,
+            INSTALLED,
+        };
+
+        private StatusEnum Status;
+
+        public EnvironmentItem(string name, StatusEnum status)
+        {
+            Name = name;
+            Status = status;
+        }
+
+        public String Name { get; set; }
+
+        public String StatusDescription {
+            get
+            {
+                if (Status == StatusEnum.INSTALLED)
+                {
+                    return "已安装";
+                }
+                else
+                {
+                    return "正在安装";
+                }
+            }
+
+            set { }
+        }
+    }
+}
