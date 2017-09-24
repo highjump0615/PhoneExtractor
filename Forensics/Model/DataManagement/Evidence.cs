014dbf9c243715d8a73c6b1a09ad6aa711387dc1
highjump <lixiang901112@163.com>
2017-09-24 17:58:43 +0800

    Added UI for evidence page

---
+++ Forensics/Model/DataManagement/Evidence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forensics.Model.DataManagement
+{
+    public class Evidence
+    {
+        public String Id { get; set; }
+
+        public String Phone { get; set; }
+        public String Name { get; set; }
+        public String Gender { get; set; }
+        public String Nation { get; set; }
+        public String IdNumber { get; set; }
+
+        public Evidence()
+        {
+            Id = "Case01";
+            Phone = "15050505050";
+            Name = "明天";
+            Gender = "男";
+            Nation = "汉";
+            IdNumber = "1010108185746131";
+        }
+    }
+}
---
+++ Forensics/Model/DataManagement/ItemsForEvidence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forensics.Model.DataManagement
+{
+    public class ItemsForEvidence : ObservableCollection<Evidence>
+    {
+        public ItemsForEvidence()
+        {
+            Add(new Evidence());
+            Add(new Evidence());
+        }
+    }
+}
