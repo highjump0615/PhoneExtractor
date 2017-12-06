13c2f3ce5e7e302c8208e00bf85d6dbd09b773da
highjump <lixiang901112@163.com>
2017-12-07 03:54:33 +0800

    Added UI for multi device dialog

---
+++ Forensics/ViewModel/Dialog/DialogMultiDeviceModel.cs
@@ -0,0 +1,37 @@
+﻿using Forensics.Model.Device;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Forensics.ViewModel.Dialog
+{
+    public class DialogMultiDeviceModel : ViewModelBase
+    {
+        public override Pages PageIndex => throw new NotImplementedException();
+
+        public List<BrandInfo> listDevice { get; set; } = new List<BrandInfo>();
+        public int SelectedIndexDevice { get; set; } = 0;
+
+        public DialogMultiDeviceModel()
+        {
+            // 添加设备
+            BrandInfo nn = new BrandInfo();
+            nn.Name = "Android device";
+            listDevice.Add(nn);
+
+            nn = new BrandInfo();
+            nn.Name = "Good Device";
+            listDevice.Add(nn);
+
+            nn = new BrandInfo();
+            nn.Name = "Do you love me?";
+            listDevice.Add(nn);
+
+            nn = new BrandInfo();
+            nn.Name = "Will you marry me?";
+            listDevice.Add(nn);
+        }
+    }
+}
